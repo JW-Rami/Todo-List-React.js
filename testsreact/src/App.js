969a0c4afbc6d2f8077193f1c2fb5ae11dd09af5
JW Rami <rami@jwcorp.fr>
2022-12-23 01:42:17 +0200

    affichage de la tache après submit

--- testsreact/src/App.js
+++ testsreact/src/App.js
@@ -1,26 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const handleButton = (e) => {
-    const bouton = e.target.value;
-    console.log(bouton);
-    console.log("bonsoir");
+  const [formState, setFormState] = useState({
+    task: "",
+  });
+  const [stateTask, setStateTask] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = {
+      task: formState.task,
+    };
+    console.log(formData);
+    setStateTask(formState.task)
   };
-  const handleChange = (e) => {
-    const bouton = e.target.value;
-    console.log(bouton);
-    console.log("bonsoir");
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
   };
+
   return (
     <>
       <section className="page-container">
-        <form>
-          <label>TACHE A FAIRE</label>
-          <input onChange={handleChange} type="text" />
-          <button>Ajouter</button>
-        </form>
-        <button onClick={handleButton}>dkopke</button>
+        <div className="todo-container">
+          <form>
+            <label>TO DO !</label>
+            <input type="text" name="task" onChange={handleChange} />
+            <button type="submit" onClick={handleSubmit}>
+              Envoyer{" "}
+            </button>
+          </form>
+          <div className="todolist-container">
+            {/* <div className="task-container">{<p>{formState.task}</p>}</div> */}
+            <div className="task-container2">{<p>{stateTask}</p>}</div>
+            
+          </div>
+          
+        </div>
       </section>
     </>
   );
