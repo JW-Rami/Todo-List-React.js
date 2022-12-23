489f8cf9a32364a28e89a0435e186ec59db95d1e
JW Rami <rami@jwcorp.fr>
2022-12-23 03:50:04 +0200

    uuid added and tasks auto generate

--- testsreact/src/App.js
+++ testsreact/src/App.js
@@ -1,20 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [formState, setFormState] = useState({
     task: "",
   });
-  const [stateTask, setStateTask] = useState("")
+  const [stateTask, setStateTask] = useState("");
+
+  const [tasks, setTasks] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
     const formData = {
+      id: uuidv4(),
       task: formState.task,
     };
     console.log(formData);
-    setStateTask(formState.task)
+    setStateTask(formState.task);
+    setTasks([...tasks, formData.task]);
   };
 
   const handleChange = (event) => {
@@ -32,14 +38,20 @@ function App() {
               Envoyer{" "}
             </button>
           </form>
-          <div className="todolist-container">
-            {/* <div className="task-container">{<p>{formState.task}</p>}</div> */}
+          {/* <div className="todolist-container">
             <div className="task-container2">{<p>{stateTask}</p>}</div>
-            
+            <div className="task-container2">{<p>{stateTask}</p>}</div>
+          </div> */}
+          <div className="todolist-container">
+            {tasks.map((task) => (
+              <div key={task.id} className="task-container">
+                <p>{task.task}</p>
+              </div>
+            ))}
           </div>
-          
         </div>
       </section>
+      {/* <div className="task-container">{<p>{formState.task}</p>}</div> */}
     </>
   );
 }
