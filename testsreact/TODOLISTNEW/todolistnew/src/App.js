b332e2e0b4d96036d3c4a9a88d004b17d4727fdc
JW Rami <rami@jwcorp.fr>
2022-12-28 21:39:24 +0200

    logic todo list

--- testsreact/TODOLISTNEW/todolistnew/src/App.js
+++ testsreact/TODOLISTNEW/todolistnew/src/App.js
@@ -1,9 +1,55 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import TodoForm from "./TodoForm/TodoForm";
+import TodoList from "./TodoList/TodoList";
+import { useEffect } from "react";
+// import Todo from "./Todo";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+  const [todos, setTodos] = useState([]);
+  // -----------------------------------
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+  // ----------------------------------
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+  function toggleComplete(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+      })
+    );
+  }
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
   return (
     <div className="App">
-      <p>React Todo</p>
+      <div className="todo-title">
+        <h1>TO DO LIST</h1>
+      </div>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        removeTodo={removeTodo}
+      />
     </div>
   );
 }
